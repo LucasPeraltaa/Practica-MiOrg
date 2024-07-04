21d31ef946fdd565e302a41465e636ff2d0180f7
LucasPeraltaa <lucasmperaltaa@gmail.com>
2024-07-03 23:44:10 -0300

    generamos componentes formulario y campoTexto, tambien pasamos props para un componente

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Header from './componentes/Header/Header';
-console.log(Header)
+import Formulario from './componentes/Formulario/Formulario';
 
 function App() {
   return (
     <div>
       <Header/>
-     
+      <Formulario/>
     </div>
   );
 }
---
+++ src/componentes/Formulario/Formulario.js
@@ -0,0 +1,15 @@
+import "./Formulario.css"
+import CampoTexto from "../CampoTexto/CampoTexto"
+
+const Formulario = () =>{
+    return <section className="formulario">
+        <form>
+            <h2>Rellena el formulario para crear el colaborador</h2>
+            <CampoTexto titulo="Nombre" placeholder="Ingresa nombre"/>
+            <CampoTexto titulo="Puesto" placeholder="Ingresa puesto"/>
+            <CampoTexto titulo="Foto" placeholder="Ingresa enlace de foto"/>
+        </form>
+    </section>
+}
+
+export default Formulario
